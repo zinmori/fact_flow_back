936cb12eb9e263f8f5c43380b30f0ec92f5f84f8
zinmori <ezechiel9087@gmail.com>
2025-08-28 07:53:13 +0000

    cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
+
 app = FastAPI(title="FactFlow Backend")
+
+# Configuration CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # À restreindre en prod
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
